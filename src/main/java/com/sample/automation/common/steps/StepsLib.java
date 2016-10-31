5dfbeb30dd29511f9d6aeb8347e835b7a81c63f0
Mitch Wright <mitchellw@perfectomobile.com>
2016-10-31 15:09:56 -0400

    Added files for reproducing switch frame bug in android

--- src/main/java/com/sample/automation/common/steps/StepsLib.java
+++ src/main/java/com/sample/automation/common/steps/StepsLib.java
@@ -3,13 +3,19 @@
  */
 package com.sample.automation.common.steps;
 
-import static com.qmetry.qaf.automation.step.CommonStep.click;
-
 import com.qmetry.qaf.automation.step.QAFTestStep;
+import com.qmetry.qaf.automation.step.QAFTestStepProvider;
+import com.qmetry.qaf.automation.ui.WebDriverTestBase;
+import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
+import com.qmetry.qaf.automation.util.StringUtil;
+import cucumber.api.java.en.Then;
+
+import static com.qmetry.qaf.automation.step.CommonStep.click;
 /**
  * @author chirag.jayswal
  *
  */
+@QAFTestStepProvider
 public class StepsLib {
 
 	@QAFTestStep(description = "open calculator")
@@ -25,6 +31,21 @@ public class StepsLib {
 		click("name="+String.valueOf(l2));
 		click("btn.equal");
 		
+	}
+
+	@Then("I switch to frame \"(.*?)\"")
+	public static void switchToFrame(String nameOrIndex) {
+		if (StringUtil.isNumeric(nameOrIndex)) {
+			int index = Integer.parseInt(nameOrIndex);
+			new WebDriverTestBase().getDriver().switchTo().frame(index);
+		} else {
+			new WebDriverTestBase().getDriver().switchTo().frame(nameOrIndex);
+		}
+	}
+
+	@Then("I switch to \"(.*?)\" frame by element")
+	public static void switchToFrameByElement(String loc) {
+		new WebDriverTestBase().getDriver().switchTo().frame(new QAFExtendedWebElement(loc));
 	}
 	
 
